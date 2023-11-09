aea6b863269a6d3e41625e9b53d664b65ca76685
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-11-09 10:59:46 +0200

    More testing

--- test.ts
+++ test.ts
@@ -1,7 +1,9 @@
 import { encodeHex } from "https://deno.land/std@0.205.0/encoding/hex.ts";
 import {
   generate_private_key_random,
+  generate_private_key_seed,
   get_public_key,
+  get_random,
   sign,
   verify,
 } from "./bindings/bindings.ts";
@@ -23,3 +25,31 @@ const msg = new TextEncoder().encode("foo bar baz");
 const signature = await sign(privateKey, msg);
 console.log("signature", encodeHex(signature));
 console.log("verified", await verify(publicKey, signature, msg) === 1);
+
+function measure<T>(fn: () => T): [T, number] {
+  const start = performance.now();
+  let end: number;
+  let res: T;
+  try {
+    res = fn();
+  } finally {
+    end = performance.now();
+  }
+  return [res, end - start];
+}
+
+for (let i = 0; i < 100; i++) {
+  const [b, t] = measure(() => {
+    return get_random(32n);
+  });
+  const [b2, t2] = measure(() => {
+    return crypto.getRandomValues(new Uint8Array(32));
+  });
+
+  console.log((await b).length);
+  console.log(b2.length);
+
+  const k = await generate_private_key_seed(await b);
+  const k2 = await generate_private_key_seed(b2);
+  console.log(t, t2);
+}
--- test_wasm.ts
+++ test_wasm.ts
@@ -4,7 +4,9 @@ import { instantiate } from "./lib/blst_deno.generated.js";
 const mod = await instantiate();
 console.log(mod);
 const {
+  get_random,
   generate_private_key_random,
+  generate_private_key_seed,
   get_public_key,
   sign,
   verify,
@@ -27,3 +29,31 @@ const msg = new TextEncoder().encode("foo bar baz");
 const signature = await sign(privateKey, msg);
 console.log("signature", encodeHex(signature));
 console.log("verified", await verify(publicKey, signature, msg) === 1);
+
+function measure<T>(fn: () => T): [T, number] {
+  const start = performance.now();
+  let end: number;
+  let res: T;
+  try {
+    res = fn();
+  } finally {
+    end = performance.now();
+  }
+  return [res, end - start];
+}
+
+for (let i = 0; i < 100; i++) {
+  const [b, t] = measure(() => {
+    return get_random(32);
+  });
+  const [b2, t2] = measure(() => {
+    return crypto.getRandomValues(new Uint8Array(32));
+  });
+
+  console.log(b.length);
+  console.log(b2.length);
+
+  const k = await generate_private_key_seed(b);
+  const k2 = await generate_private_key_seed(b2);
+  console.log(t, t2);
+}
